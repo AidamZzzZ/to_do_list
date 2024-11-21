23b49ea9f96acd24a2e4d17b83efef63e44c7ff8
AidamZzzZ <adrian_alliskair@hotmail.com>
2024-11-21 00:04:30 -0400

    commit three

--- to_do_app/urls.py
+++ to_do_app/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('task.urls')),
     path('admin/', admin.site.urls),
-    path('task/', include('task.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
