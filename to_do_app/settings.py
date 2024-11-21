cfe07ef9ce3f1bc407e19c948cf654d3e4ce2b2f
AidamZzzZ <adrian_alliskair@hotmail.com>
2024-11-20 23:50:24 -0400

    commit urls

--- to_do_app/settings.py
+++ to_do_app/settings.py
@@ -91,11 +91,7 @@ WSGI_APPLICATION = 'to_do_app.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    'default': dj_database_url.config(os.environ.get('DATABASE_URL'))
 }
 
 
